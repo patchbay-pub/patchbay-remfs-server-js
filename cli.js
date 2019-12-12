5772988b462aa1ed48e7894283ca05c6bf43827b
Anders Pitman <tapitman11@gmail.com>
2019-12-12 10:24:08 -0700

    webFS reneamed to remFS

--- cli.js
+++ cli.js
@@ -2,18 +2,18 @@
 
 const process = require('process');
 const http = require('patchbay-http');
-const { createHandler } = require('webfs-server');
+const { createHandler } = require('remfs-server');
 
 if (process.argv.length < 3) {
-  console.log("Usage: patchbay-webfs-server ROOT_CHANNEL");
+  console.log("Usage: patchbay-remfs-server ROOT_CHANNEL");
   process.exit(1);
 }
 
 const rootChannel = process.argv[2];
 const rootPath = '/req' + rootChannel;
 
-const webfsHandler = createHandler({ rootPath });
-const httpServer = http.createServer(webfsHandler);
+const remfsHandler = createHandler({ rootPath });
+const httpServer = http.createServer(remfsHandler);
 httpServer.setPatchbayServer('https://patchbay.pub');
 httpServer.setPatchbayChannel(rootChannel);
 httpServer.listen();
